f6859f14c108ac1d2f80a38597b347ac4f4aa822
Britnie-Ann Gray <britnieanngrayy@gmail.com>
2024-10-28 00:02:00 -0500

    All changes made

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,17 +9,42 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Squares:", squares.length); 
     console.log("Class squares:", document.getElementsByClassName("square").length);
 
-
-    let boxes = Array.from(document.getElementsByClassName('square'))
     
-    //Adding 'X' or 'O' to box spaces
+    //Variables
     const Xtext = "X"
     const Otext = "O"
     let currentPlayer = Xtext
+    let stat = document.getElementById('status')
+    let newBtn = document.getElementsByClassName('btn')[0]
     let spaces = Array(9).fill(null)
     console.log(spaces)
+    //Winner checking variable
+    const winner = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
 
-        
+    //Functions
+    //This checks if a player is a winner bassed on certain winning combinations
+    function winnerCheck() {
+        for (const condition of  winner) {
+            let [a,b,c] = condition
+
+            //checking if the letter at each winning pposiible position is the same
+            if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])){
+                return [a,b,c]
+            }
+        }
+        return false
+    }
+     
+    //What to do when a square is selected
     function selectedSquare(e) {
         const square = e.target
         const index = Array.from(squares).indexOf(square)
@@ -28,7 +53,13 @@ document.addEventListener("DOMContentLoaded", function() {
         if (!spaces[index]){
             e.target.innerText = currentPlayer
             spaces[index] = currentPlayer
+            
+            if(winnerCheck() !==false){
+                stat.textContent = 'Congratulations! ' + currentPlayer + ' is the Winner!'
+                stat.className = "you-won"
 
+                return
+            }
             if (currentPlayer == Xtext){
                 e.target.innerText.className = "square X"
                 currentPlayer = Otext
@@ -53,12 +84,27 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    function resetGame() {
+        console.log("reseting game")
+        spaces.fill(null)
+
+        squares.forEach(square => {
+            square.innerText = " "
+            square.className = "square"
+        })
+
+        stat.textContent = "Move your mouse over a square and click to play an X or an O."
+        stat.classList.remove("you-won")
+        currentPlayer = Xtext
+    }
 
 
+//Event Listeners
 squares.forEach((square) => {
     square.addEventListener('click', selectedSquare)
     square.addEventListener("mouseover", HoverOverSQ)
 }
 )
 
+newBtn.addEventListener('click', resetGame)
 })
