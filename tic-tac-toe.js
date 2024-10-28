7eae8d4829dd981a09ea4f41ed505e5fd011fce5
Britnie-Ann Gray <britnieanngrayy@gmail.com>
2024-10-27 22:12:52 -0500

    Added X and O mechanics

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,8 +5,42 @@ document.addEventListener("DOMContentLoaded", function() {
     squares.forEach(square => {
         square.className = "square";
     });
-});
 
-//To ensure all div boxes are accounted for
-let squares2 = document.getElementsByClassName("square");
-console.log(squares2);
+    console.log("Squares:", squares.length); 
+    console.log("Class squares:", document.getElementsByClassName("square").length);
+
+
+    let boxes = Array.from(document.getElementsByClassName('square'))
+    
+    //Adding 'X' or 'O' to box spaces
+    const Xtext = "X"
+    const Otext = "O"
+    let currentPlayer = Xtext
+    let spaces = Array(9).fill(null)
+    console.log(spaces)
+
+        
+    function selectedSquare(e) {
+        const square = e.target
+        const index = Array.from(squares).indexOf(square)
+        console.log(e.target)
+
+        if (!spaces[index]){
+            e.target.innerText = currentPlayer
+            spaces[index] = currentPlayer
+
+            if (currentPlayer == Xtext){
+                Xtext.className = "square X"
+                currentPlayer = Otext
+            }
+            else if (currentPlayer == Otext){
+                Otext.className = "square O"
+                currentPlayer = Xtext
+            }
+        }
+}
+
+squares.forEach(square => {
+    square.addEventListener('click', selectedSquare)}
+)
+});
